166055ef88063af584d9c2abaf9449828eeb2d1f
egomez <egomez@gt99.com.ve>
2023-10-13 16:11:37 -0400

    Agregar archivos de proyecto.

---
+++ ConsoleAppLeetCode/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleAppLeetCode;
+
+Console.WriteLine("=====================");
+Console.WriteLine("TwoSum");
+var array = new int[] { 2, 4, 7, 8 };
+int target = 9;
+Console.WriteLine($"Array {array[0]}, {array[1]}, {array[2]}, {array[3]}, Target:{target}");
+var result = TwoSumChallenge.TwoSum(array, target);
+Console.WriteLine($"{result[0]}, {result[1]}");
+Console.WriteLine("=====================");
