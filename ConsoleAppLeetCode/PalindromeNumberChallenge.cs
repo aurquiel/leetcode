86f86c57fedf43bb062823c2423fee72f5ebcd86
egomez <edgargomez.ucv@gmail.com>
2023-10-13 21:59:39 -0400

    Add PalidromeNumber

---
+++ ConsoleAppLeetCode/PalindromeNumberChallenge.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppLeetCode
+{
+    public static class PalindromeNumberChallenge
+    {
+        public static bool IsPalindrome(int x)
+        {
+            int reverted = 0, save = x;
+            while (save > 0)
+            {
+                reverted = reverted * 10 + save % 10;
+                save = save / 10;
+            }
+
+            return reverted == x;
+        }
+    }
+}
--- ConsoleAppLeetCode/Program.cs
+++ ConsoleAppLeetCode/Program.cs
@@ -9,3 +9,10 @@ Console.WriteLine($"Array {array[0]}, {array[1]}, {array[2]}, {array[3]}, Target
 var result = TwoSumChallenge.TwoSum(array, target);
 Console.WriteLine($"{result[0]}, {result[1]}");
 Console.WriteLine("=====================");
+
+Console.WriteLine("=====================");
+Console.WriteLine("PalindromeNumber");
+Console.WriteLine(PalindromeNumberChallenge.IsPalindrome(512225));
+Console.WriteLine(PalindromeNumberChallenge.IsPalindrome(5225));
+Console.WriteLine(PalindromeNumberChallenge.IsPalindrome(-121));
+Console.WriteLine("=====================");
